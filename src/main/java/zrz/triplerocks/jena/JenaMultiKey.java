a945d870f726265759b034b163c906fcbb9a56f4
Theo Zourzouvillys <theo@zrz.io>
2018-10-19 15:51:13 -0700

    store datatype with literal nodes
    will allow support for indexing on specific types soon

--- src/main/java/zrz/triplerocks/jena/JenaMultiKey.java
+++ src/main/java/zrz/triplerocks/jena/JenaMultiKey.java
@@ -20,21 +20,34 @@ class JenaMultiKey {
   private static final byte IRI_TYPE = 2;
   private static final byte VALUE_TYPE = 3;
 
+  // literal seperator. can't be in the key.
+  private static final byte[] DATATYPE_SEPERATOR = new byte[] { 1 };
+
   private static final byte[] BN_PFX = new byte[] { BN_TYPE };
   private static final byte[] IRI_PFX = new byte[] { IRI_TYPE };
   private static final byte[] VALUE_PFX = new byte[] { VALUE_TYPE };
 
   public static byte[] toKey(final Node s) {
+
     if (s.isBlank()) {
       return Bytes.concat(BN_PFX, ((Node_Blank) s).getBlankNodeLabel().getBytes());
     }
-    else if (s.isLiteral()) {
+
+    if (s.isLiteral()) {
+      final String dtype = s.getLiteralDatatypeURI();
+      if (dtype != null) {
+        return Bytes.concat(VALUE_PFX, s.getLiteralLexicalForm().getBytes(), DATATYPE_SEPERATOR, dtype.getBytes());
+      }
+      // untyped.
       return Bytes.concat(VALUE_PFX, s.getLiteralLexicalForm().getBytes());
     }
-    else if (s.isURI()) {
+
+    if (s.isURI()) {
       return Bytes.concat(IRI_PFX, ((Node_URI) s).toString().getBytes(UTF_8));
     }
+
     throw new UnsupportedOperationException("invalid type: " + s.getClass());
+
   }
 
   public static Triple fromKey(final IndexKind idx, final byte[] key) {
@@ -60,12 +73,40 @@ class JenaMultiKey {
         return NodeFactory.createBlankNode(new String(data, start + 1, len - 1, UTF_8));
       case IRI_TYPE:
         return NodeFactory.createURI(new String(data, start + 1, len - 1, UTF_8));
-      case VALUE_TYPE:
-        return NodeFactory.createLiteral(new String(data, start + 1, len - 1, UTF_8));
+      case VALUE_TYPE: {
+        // find the null char, which is our datatype.
+
+        final int idx = findSeperator(data, start, len);
+
+        if (idx == -1) {
+          // untyped
+          return NodeFactory.createLiteral(new String(data, start + 1, len - 1, UTF_8));
+        }
+
+        final int valueStart = start + 1;
+        final int dataTypeStart = idx + 1;
+        final int end = start + len;
+
+        // the datatype ...
+        final String literalValue = new String(data, valueStart, dataTypeStart - valueStart - 1, UTF_8);
+        final String dataType = new String(data, dataTypeStart, end - dataTypeStart, UTF_8);
+
+        return NodeFactory.createLiteral(literalValue, NodeFactory.getType(dataType));
+
+      }
       default:
         throw new UnsupportedOperationException("invalid type: " + Byte.toUnsignedInt(data[start]));
     }
 
+  }
+
+  private static int findSeperator(final byte[] data, final int start, final int len) {
+    for (int i = 0; i < (len - 1); ++i) {
+      if (data[start + i] == DATATYPE_SEPERATOR[0]) {
+        return start + i;
+      }
+    }
+    return -1;
   }
 
 }
